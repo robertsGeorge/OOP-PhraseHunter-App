f0f2b39c16c6bfdbae66c634aa3d1e58203740bd
George Roberts <george.e.roberts@gmail.com>
2019-05-17 09:21:34 +0100

    draft skeleton of class Game

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,42 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed = 0;
+    this.phrases = [/* array of five Phrase objects; letters and spaces only */];
+    this.activePhrase = null;
+  }
+
+
+  startGame() {
+
+  }
+
+
+  getRandomPhrase() {
+
+  }
+
+
+  handleInteraction() {
+
+  }
+
+
+  removeLife() {
+
+  }
+
+
+  checkForWin() {
+
+  }
+
+  
+  gameOver() {
+
+  }
+
+}
