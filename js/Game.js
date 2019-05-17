12b9592cb74de16aac8aafc73ab32aece6029225
George Roberts <george.e.roberts@gmail.com>
2019-05-17 17:41:18 +0100

    write createPhrases() method on Game class

--- js/Game.js
+++ js/Game.js
@@ -6,9 +6,26 @@ class Game {
   constructor() {
     this.activePhrase = null;
     this.missed = 0;
-    this.phrases = [/* array of five Phrase objects; letters and spaces only */];
+    this.phrases = this.createPhrases();
   }
 
+  /**
+   * Creates phrases for use in game
+   * @return {array}  phraseObjects  -   An array of phrases that could be used in the game.
+   */
+  createPhrases() {
+    const phraseObjects = [];
+    
+    // Access each phrase (string) in phraseList array in phraseList.js
+    phraseList.forEach(phrase => {
+      // Instantiate new Phrase object, passing in phrase.
+      // Then push to phraseObjects array.
+      phraseObjects.push(new Phrase(phrase));
+    });
+
+    // push each phrase object into an array, then return that array
+    return phraseObjects;
+  }
 
   startGame() {
 
