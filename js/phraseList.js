ae2a56d1096820cbcfacb9fe313d9134f2bf9357
George Roberts <george.e.roberts@gmail.com>
2019-05-17 13:13:25 +0100

    add phraseList.js (forgot to track)

---
+++ js/phraseList.js
@@ -0,0 +1,14 @@
+const phraseList = [
+  'seize the day',
+  'piece of cake',
+  'back to square one',
+  'cut to the chase',
+  'an arm and a leg',
+  'off the record',
+  'a sight for sore eyes',
+  'cool as a cucumber',
+  'busy as a bee',
+  'bring home the bacon',
+  'two peas in a pod',
+  'saved by the bell'
+];
